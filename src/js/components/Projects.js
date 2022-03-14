d757b434f222fffd1169a14985dd7034fa7802b0
Madison Mitchell <madisonrosemitchell@gmail.com>
2022-03-14 10:27:53 -0400

    WIP

--- src/js/components/Projects.js
+++ src/js/components/Projects.js
@@ -2,7 +2,7 @@ import ohioansCare from "../../images/ohioansCare.png"
 
 export default function Projects() {
     return `
-    <div id="ohioansCare" class="project header-wrapper border-solid border-2 border-cyan-500 rounded-3xl my-5 animate__animated animate__bounceInDown bg-gradient-to-r from-cyan-600 to-blue-600 cursor-pointer">
+    <div id="ohioansCare" class="project header-wrapper border-solid border-2 border-cyan-500 rounded-3xl my-5 animate__animated animate__bounceInDown bg-gradient-to-r from-cyan-200 to-blue-200 cursor-pointer">
         <div class="project-container-left rounded-3xl">
             <div class="photo rounded-3xl text-center">
                 <img src="${ohioansCare}" alt="Madison Mitchell" class="website-img rounded-3xl m-auto hover:from-pink-500 hover:to-yellow-500">
@@ -11,41 +11,41 @@ export default function Projects() {
             <div class="header-grid m-auto flex flex-col">
                 <div class="header justify-center flex  mx-6">
                     <span class="html-text align-left align-top text-sky-300">&lt;header&gt;</span>
-                    <div class="title text-center p-3 text-sky-50 font-cursive leading-10">
+                    <div class="title text-center p-3 text-gray-900 font-cursive hover:text-cyan-500 active:text-cyan-700 leading-10">
                         Ohioans Care
                     </div>
-                    <span class="html-text align-right align-bottom text-sky-300">&lt;/header&gt;</span>
+                    <span class="html-text align-right align-bottom text-blue-300">&lt;/header&gt;</span>
                 </div>
 
                 <div class="header justify-center flex  mx-6">
                     <span class="html-text align-left align-top text-sky-300">&lt;h1&gt;</span>
-                    <div class="sub-heading-2 text-justify tracking-wider font-mono p-3 text-sky-100">
+                    <div class="sub-heading-2 text-justify tracking-wider font-mono p-3 text-gray-900">
                         Ohioans Care is built with the intention of connecting Ohioans to the help they need via government social services or local community outreach. Our mission is to create an outstanding social care network within the state of Ohio and uplift the lives of people in need.
                     </div>
-                    <span class="html-text align-right align-bottom text-sky-300">&lt;/h1&gt;</span>
+                    <span class="html-text align-right align-bottom text-blue-300">&lt;/h1&gt;</span>
                 </div>
             </div>
         </div>
     </div>
 
-    <div id="mysteryEducator" class="project header-wrapper border-solid border-2 border-blue-500 rounded-3xl my-5 animate__animated animate__bounceInDown animate__delay-1s bg-gradient-to-r from-cyan-600 to-blue-600">
+    <div id="mysteryEducator" class="project header-wrapper border-solid border-2 border-blue-500 rounded-3xl my-5 animate__animated animate__bounceInDown animate__delay-1s bg-gradient-to-r from-cyan-200 to-blue-200">
         <div class="project-container-right rounded-3xl">
             <div class="header-grid m-auto flex flex-col">
                 <div class="header justify-center flex  mx-6">
                     <span class="html-text align-left align-top text-sky-300">&lt;header&gt;</span>
-                    <div class="title text-center p-3 text-sky-50 font-cursive leading-10">
-                    <span class="html-text align-left align-top text-sky-300">JAC's</span><br>
+                    <div class="title text-center p-3 text-gray-900 font-cursive hover:text-sky-500 active:text-sky-700 leading-10">
+                    <span class="html-text align-left align-top text-gray-900">JAC's</span><br>
                         Mystery Educator
                     </div>
-                    <span class="html-text align-right align-bottom text-sky-300">&lt;/header&gt;</span>
+                    <span class="html-text align-right align-bottom text-blue-300">&lt;/header&gt;</span>
                 </div>
 
                 <div class="header justify-center flex mx-6">
                     <span class="html-text align-left align-top text-sky-300">&lt;h1&gt;</span>
-                    <div class="sub-heading-2 text-justify p-3 text-sky-100 font-mono">
+                    <div class="sub-heading-2 text-justify p-3 text-gray-900 font-mono">
                         Navigating the new world of home education has been challenging for all of us. Mystery Educator is an education application that presents various aspects of science, art, and history that involves children in the learning process while keeping them engaged.
                     </div>
-                    <span class="html-text align-right align-bottom text-sky-300">&lt;/h1&gt;</span>
+                    <span class="html-text align-right align-bottom text-blue-300">&lt;/h1&gt;</span>
                 </div>
             </div>
 
@@ -55,7 +55,7 @@ export default function Projects() {
         </div>
     </div>
 
-    <div id="islandHopping" class="project header-wrapper border-solid border-2 border-cyan-500 rounded-3xl my-5 animate__animated animate__bounceInDown animate__delay-2s bg-gradient-to-r from-cyan-600 to-blue-600">
+    <div id="islandHopping" class="project header-wrapper border-solid border-2 border-cyan-500 rounded-3xl my-5 animate__animated animate__bounceInDown animate__delay-2s bg-gradient-to-r from-cyan-200 to-blue-200">
         <div class="project-container-left rounded-3xl">
             <div class="photo rounded-3xl justify-around flex">
                 <img src="${ohioansCare}" alt="Contact" class="website-img rounded-3xl m-auto">
@@ -64,19 +64,19 @@ export default function Projects() {
             <div class="header-grid m-auto flex flex-col  mx-6">
                 <div class="header justify-center flex">
                     <span class="html-text align-left align-top text-sky-300">&lt;header&gt;</span>
-                    <div class="title text-center p-3 text-sky-50 font-cursive leading-10">
-                    <span class="html-text align-left align-top text-sky-300">JAC's</span><br>
+                    <div class="title text-center p-3 text-gray-900 font-cursive hover:text-blue-500 active:text-blue-700 leading-10">
+                    <span class="html-text align-left align-top text-gray-900">JAC's</span><br>
                         Island Hopping Adventures
                     </div>
-                    <span class="html-text align-right align-bottom text-sky-300">&lt;/header&gt;</span>
+                    <span class="html-text align-right align-bottom text-blue-300">&lt;/header&gt;</span>
                 </div>
 
                 <div class="header justify-center flex  mx-6">
                     <span class="html-text align-left align-top text-sky-300">&lt;h1&gt;</span>
-                    <div class="sub-heading-2 text-justify p-3 text-sky-100 font-mono">
+                    <div class="sub-heading-2 text-justify p-3 text-gray-900 font-mono">
                         JAC's Island Hopping Adventures website was created with the intention of educating current and future clientele of what island hopping is and island hopping adventures offered by JAC Enterprises.
                     </div>
-                    <span class="html-text align-right align-bottom text-sky-300">&lt;/h1&gt;</span>
+                    <span class="html-text align-right align-bottom text-blue-300">&lt;/h1&gt;</span>
                 </div>
             </div>
         </div>
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-// import 'animate.css';
+import 'animate.css';
 import Home from "./components/Home.js";
 import About from "./components/About.js";
 import Contact from "./components/Contact.js";
@@ -14,12 +14,17 @@ function buildPage() {
 
 function renderHome() {
     app.innerHTML = Home();
-    
+
+    navBtns()
+}
+
+function navBtns() {
     navHome();
     navContact();
-    navAbout();
+    // navAbout();
     navProjects();
-    clickFadeOut()
+    clickFadeOut();
+    scrollToTop()
 }
 
 function navHome() {
@@ -27,72 +32,111 @@ function navHome() {
 
     homeNavBtn.addEventListener("click", () => {
         app.innerHTML = Home();
-
-        navAbout();
-        navProjects();
-        navContact();
+        navBtns();
     });
 }
 
-function navAbout() {
-    const aboutElem = document.getElementById("madison");
-    
-    aboutElem.addEventListener("click", () => {
-        console.log("hi")
-        app.innerHTML = About();
-    });
-}
+// function navAbout() {
+//     const aboutElem = document.getElementById("madison");
+//     const aboutBtn = document.querySelector(".about");
+
+//     aboutElem.addEventListener("click", () => {
+//         app.innerHTML = About();
+//         navBtns();
+//     });
+
+//     aboutBtn.addEventListener("click", () => {
+//         app.innerHTML = About();
+//         navBtns();
+//     });
+// }
 
 function navProjects() {
-    const projectsElem = document.getElementById("projects")
-    
+    const projectsElem = document.getElementById("projects");
+    const projectsBtn = document.querySelector(".projects");
+
     projectsElem.addEventListener("click", () => {
         app.innerHTML = Projects();
+        navBtns();
+    });
+
+    projectsBtn.addEventListener("click", () => {
+        app.innerHTML = Projects();
+        navBtns();
     });
 }
 
 function navContact() {
+    const contactElem = document.getElementById("contact");
     const contactNavBtn = document.querySelector(".contact");
+
+    contactElem.addEventListener("click", () => {
+        app.innerHTML = Contact();
+        navBtns();
+    });
 
     contactNavBtn.addEventListener("click", () => {
         app.innerHTML = Contact();
+        navBtns();
+    });
+}
+
+function scrollToTop() {
+    const handPointUp = document.querySelector(".fa-hand-point-up");
+
+    handPointUp.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
     });
 }
 
 function clickFadeOut() {
+    const navElem = document.querySelector(".nav-wrapper");
     const aboutElem = document.getElementById("madison");
     const projectElem = document.getElementById("projects");
     const contactElem = document.getElementById("contact");
     const footerElem = document.getElementById("footer");
 
     aboutElem.addEventListener("click", () => {
+        navElem.classList.remove("animate__delay-1s");
         aboutElem.classList.remove("animate__delay-1s");
         projectElem.classList.remove("animate__delay-1s");
         contactElem.classList.remove("animate__delay-1s");
         footerElem.classList.remove("animate__delay-1s");
+
+        navElem.classList.remove("animate__delay-2s");
         aboutElem.classList.remove("animate__delay-2s");
         projectElem.classList.remove("animate__delay-2s");
         contactElem.classList.remove("animate__delay-2s");
         footerElem.classList.remove("animate__delay-2s");
+
+        navElem.classList.remove("animate__delay-3s");
         aboutElem.classList.remove("animate__delay-3s");
         projectElem.classList.remove("animate__delay-3s");
         contactElem.classList.remove("animate__delay-3s");
         footerElem.classList.remove("animate__delay-3s");
 
+        navElem.classList.remove("animate__fadeInDown");
         footerElem.classList.remove("animate__fadeInUp");
 
-        aboutElem.classList.add("animate__fadeOut");
-        projectElem.classList.add("animate__fadeOut");
-        contactElem.classList.add("animate__fadeOut");
+        navElem.classList.add("animate__fadeOut");
+        aboutElem.classList.add("animate__zoomOut");
+        projectElem.classList.add("animate__zoomOutUp");
+        contactElem.classList.add("animate__zoomOutDown");
         footerElem.classList.add("animate__fadeOut");
 
-        
-        setTimeout(function() {
-            app.innerHTML = About(); 
+
+        setTimeout(function () {
+            app.innerHTML = About();
+            navElem.classList.remove("animate__fadeOut");
+            navElem.classList.add("animate__fadeInDownBig");
+            navElem.classList.add("animate__delay-3s");
             footerElem.classList.remove("animate__fadeOut");
             footerElem.classList.add("animate__fadeInUpBig");
-            footerElem.classList.add("animate__delay-2s");
-        }, 1500);
-        
+            footerElem.classList.add("animate__delay-3s");
+        }, 1250);
+
     });
-  }
+}
